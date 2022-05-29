be43e4e4e4b35b467e8edb30cdfcf4e939fe70bf
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-05-29 14:17:06 +0600

    v1.1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ console.log("main app js");
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw_cached_pages.js')
+      .register('../sw_cached_pages.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
