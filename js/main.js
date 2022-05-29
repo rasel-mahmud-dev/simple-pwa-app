7570236c3df05cbfa5ea7f5b3bc0554702c14950
rasel mahmud dev <rasel.mahmud.dev@gmail.com>
2022-05-29 14:11:00 +0600

    v1.1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+
+console.log("main app js");
+
 // Make sure sw are supported
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_pages.js')
+      .register('/sw_cached_pages.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
